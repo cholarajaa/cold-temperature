4e7e35fd951d62c0f8febed0382f0c31ada0980f
Madhu Reddy <madhureddy88@gmail.com>
2017-08-21 00:37:12 +0530

    Hello reactorgit status -s!

--- hotorcold/hotorcold/urls.py
+++ hotorcold/hotorcold/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from reactor.views import index
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('reactor.urls')),
 ]

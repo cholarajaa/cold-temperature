70a6456fce7499740096c556a6fc89811f0966b3
Madhu Reddy <madhureddy88@gmail.com>
2017-08-21 08:52:57 +0530

    creates data models

--- hotorcold/reactor/models.py
+++ hotorcold/reactor/models.py
@@ -3,4 +3,96 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class EventsDump(models.Model):
+    '''To store raw dump of the events'''
+    events_json = models.TextField(null=False, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        super(EventsDump, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.events_json
+
+
+class UserData(models.Model):
+    """To store user related data"""
+    owner_name = models.CharField(max_length=256)
+    company = models.CharField(max_length=256, null=False, blank=False)
+    usertype = models.CharField(max_length=256, null=False, blank=False)
+    partnerName = models.CharField(max_length=256, null=True, blank=True)
+    segmentTypeDeparture = models.CharField(
+        max_length=256, null=True, blank=True)
+    functionalName = models.CharField(max_length=256, null=True, blank=True)
+    partnerTypeStart = models.CharField(max_length=256, null=True, blank=True)
+    bizLocationTypeStart = models.CharField(
+        max_length=256, null=True, blank=True)
+    packagingTypeCode = models.CharField(max_length=256, null=True, blank=True)
+    tradeItemCountryOfOrigin = models.CharField(
+        max_length=256, null=False, blank=False)
+    lowTemp = models.IntegerField(null=False, blank=False)
+    referenceTemp = models.IntegerField(null=False, blank=False)
+    referenceLife = models.IntegerField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '%s - %s - %s' % (self.owner_name, self.company, self.usertype)
+
+    class Meta:
+        unique_together = (
+            'owner_name', 'company', 'usertype'
+        )
+
+
+class Event(models.Model):
+    '''To store events'''
+    time = models.DateTimeField(null=False, blank=False)
+    organization_id = models.IntegerField(
+        null=False, blank=False)
+    raspberry_id = models.CharField(
+        max_length=256, null=False, blank=False)
+    event_type = models.CharField(
+        max_length=256, null=False, blank=False)
+    event_order = models.IntegerField(
+        null=False, blank=False)
+    is_current = models.BooleanField()
+    elapsed_seconds = models.IntegerField(null=False, blank=False)
+    company_name = models.CharField(max_length=256, null=True, blank=True)
+    client = models.CharField(max_length=256, null=True, blank=True)
+    read_point = models.CharField(max_length=256, null=True, blank=True)
+    biz_location = models.CharField(max_length=256, null=True, blank=True)
+    biz_step = models.CharField(max_length=256, null=True, blank=True)
+    version = models.IntegerField()
+    user_data = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '%s - %d - %s - %s - %d' % (
+            self.time, self.organization_id, self.raspberry_id,
+            self.event_type, self.event_order)
+
+    class Meta:
+        unique_together = (
+            'time', 'organization_id', 'raspberry_id',
+            'event_type', 'event_order'
+        )
+
+
+class AggergatedUserData(models.Model):
+    """Aggregated data from UserData table"""
+    company = models.CharField(max_length=256, null=False, blank=False)
+    usertype = models.CharField(max_length=256, null=False, blank=False)
+    lowTemp = models.FloatField(null=True, blank=True)
+    referenceTemp = models.FloatField(null=True, blank=True)
+    referenceLife = models.FloatField(null=True, blank=True)
+
+    def __unicode__(self):
+        return '%s - %s' % (
+            self.company, self.usertype)
+
+    class Meta:
+        unique_together = ('company', 'usertype')
---
+++ hotorcold/reactor/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from heat.models import Event, UserData, EventsDump
+
+
+class EventsDumpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventsDump
+        fields = '__all__'
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserData
+        fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.partnerName = validated_data.get(
+            'partnerName', instance.partnerName)
+        instance.segmentTypeDeparture = validated_data.get(
+            'segmentTypeDeparture', instance.segmentTypeDeparture)
+        instance.functionalName = validated_data.get(
+            'functionalName', instance.functionalName)
+        instance.partnerTypeStart = validated_data.get(
+            'partnerTypeStart', instance.partnerTypeStart)
+        instance.bizLocationTypeStart = validated_data.get(
+            'bizLocationTypeStart', instance.bizLocationTypeStart)
+        instance.packagingTypeCode = validated_data.get(
+            'packagingTypeCode', instance.packagingTypeCode)
+        instance.tradeItemCountryOfOrigin = validated_data.get(
+            'tradeItemCountryOfOrigin', instance.tradeItemCountryOfOrigin)
+        instance.lowTemp = validated_data.get('lowTemp', instance.lowTemp)
+        instance.referenceTemp = validated_data.get(
+            'referenceTemp', instance.referenceTemp)
+        instance.referenceLife = validated_data.get(
+            'referenceLife', instance.referenceLife)
+        instance.save()
+        return instance
+
+
+class EventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user_data = validated_data.get('user_data')
+        user_ser = UserDataSerializer(data=user_data)
+        if user_ser.is_valid():
+            user_data = user_ser.save()
+        validated_data['user_data'] = user_data
+        event = Event.objects.create(**validated_data)
+        return event
+
+    def update(self, instance, validated_data):
+        instance.read_point = validated_data.get(
+            'read_point', instance.read_point)
+        instance.version = validated_data.get(
+            'version', instance.version)
+        instance.biz_location = validated_data.get(
+            'biz_location', instance.biz_location)
+        instance.elapsed_seconds = validated_data.get(
+            'elapsed_seconds', instance.elapsed_seconds)
+        instance.client = validated_data.get('client', instance.client)
+        instance.is_current = validated_data.get(
+            'is_current', instance.is_current)
+        instance.biz_step = validated_data.get(
+            'biz_step', instance.biz_step)
+        instance.company_name = validated_data.get(
+            'company_name', instance.company_name)
+        instance.save()
+        return instance
